c6cb8b750ac4856204e59b45f07c95ed00cc8e41
Edward Sager <ed@sagers.org>
2021-05-08 17:28:21 +0000

    Ongoing dev

--- server.01.js
+++ server.01.js
@@ -7,7 +7,7 @@ if (process.env["NODE_PATH"]) {
     }
 }
 const http = require("http");
-const tracer = require("tracer").console({ transport: function (data) { process.stdout.write(data.output + "\n") }, format: "{{timestamp}} {{file}}:{{line}} {{method}} {{message}}", dateformat: "HH:MM:ss.l", });
+const tracer = require("tracer").console({ transport: function(data) { process.stdout.write(data.output + "\n") }, format: "{{timestamp}} {{file}}:{{line}} {{method}} {{message}}", dateformat: "HH:MM:ss.l", });
 const util = require("util");
 const fs = require("fs");
 const md5 = require("md5");
@@ -15,30 +15,33 @@ const mime = require('mime-types');
 
 let staticDir = 'static';
 
-let handler = function (req, res) {
+let handler = function(req, res) {
     try {
-        //tracer.log(JSON.stringify(req.headers,null,4));
         let urlDerived = 'http://' + req.headers.host + req.url;
-        //tracer.log(urlDerived);
         req.urlParsed = parseURLextended(urlDerived);
-        //tracer.log(JSON.stringify(url,null,4));
+        tracer.log(req.urlParsed.path);
+        if (req.urlParsed.path == '' || req.urlParsed.path == '/') {
+            req.urlParsed.path = '/index.html';
+        }
+        tracer.log(req.urlParsed.path);
         let staticFullPath = staticDir + req.urlParsed.path;
         tracer.log(staticFullPath);
         if (endpoints[req.urlParsed.path] && typeof endpoints[req.urlParsed.path] == 'function') {
-            endpoints[req.urlParsed.path](req,res);
-        }
-        else if (fs.existsSync(staticFullPath)) {
+            endpoints[req.urlParsed.path](req, res);
+            return;
+            // logging here
+        } else if (fs.existsSync(staticFullPath)) {
             let contentType = mime.lookup(staticFullPath);
             let stream = fs.createReadStream(staticFullPath);
             stream.on('error', (err) => {
                 try {
                     res.setHeader("Content-Type", 'text/plain');
-                    res.writeHead(500, header);
+                    res.writeHead(500);
                     res.end(error.message)
-                }
-                catch(error) {
+                    return;
+                } catch (error) {
                     tracer.log(error.message + "\n" + error.stack);
-                    res.writeHead(500, header);
+                    //res.writeHead(500);
                     res.end(error.message);
                     return;
                 }
@@ -49,29 +52,56 @@ let handler = function (req, res) {
             res.setHeader("Content-Type", contentType);
             res.writeHead(200);
             stream.pipe(res);
-        }
-        else {
+            return;
+        } else {
             res.setHeader("Content-Type", "text/plain");
             res.writeHead(404);
-            res.end(JSON.stringify(req.urlParsed,null,4));
+            res.end(JSON.stringify(req.urlParsed, null, 4));
+            return;
+            //logging here
         }
-    }
-    catch(error) {
-        tracer.log(error.message+"\n"+error.stack);
+    } catch (error) {
+        tracer.log(error.message + "\n" + error.stack);
         res.setHeader("Content-Type", "text/plain");
         res.writeHead(500);
-        res.end(error.message+"\n"+error.stack);
+        res.end(error.message + "\n" + error.stack);
+        return;
     }
 };
 
 let endpoints = {
-    '/api/url' : url
+    '/api/url': url,
+    '/api/employer/login': loginEmployer,
+    '/api/jobseeker/login': loginJobSeeker,
 }
 
-function url(req,res) {
+function url(req, res) {
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
-    res.end(JSON.stringify(req.urlParsed,null,4));
+    res.end(JSON.stringify(req.urlParsed, null, 4));
+}
+
+function loginEmployer(req, res) {
+    let result = { result: 'OK', url: req.urlParsed };
+    try {
+        if (!req.urlParsed.params.emailAddress.match(/@/)) {
+            result.result = 'FAIL';
+            result.error = "E-Mail Address doesn't look right, it's missing an @";
+        }
+    } catch (error) {
+        result.result = 'FAIL';
+        result.error = error.message;
+        result.stack = error.stack;
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(result, null, 4));
+}
+
+function loginJobSeeker(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(req.urlParsed, null, 4));
 }
 
 const host = '0.0.0.0';
@@ -81,77 +111,72 @@ server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
 
-function parseURLextended(url,origin) {
+function parseURLextended(url, origin) {
     try {
-            let parsedURLObj = new URL(url,origin);
-            //tracer.log(parsedURLObj);
-            let parsedURL = {};
-            for (let k in parsedURLObj) {
-                    let temp = parsedURLObj[k];
-                    if (typeof temp == "string") {
-                            if (k == "origin") {
-                                    parsedURL.index = temp.replace(/^http:|https:/, "");
-                                    parsedURL[k] = temp;
-                            } else {
-                                    parsedURL[k] = temp;
-                            }
-                    }
+        let parsedURLObj = new URL(url, origin);
+        //tracer.log(parsedURLObj);
+        let parsedURL = {};
+        for (let k in parsedURLObj) {
+            let temp = parsedURLObj[k];
+            if (typeof temp == "string") {
+                if (k == "origin") {
+                    parsedURL.index = temp.replace(/^http:|https:/, "");
+                    parsedURL[k] = temp;
+                } else {
+                    parsedURL[k] = temp;
+                }
             }
-            // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
-            parsedURL.params = {};
-            for (let pair of parsedURLObj.searchParams.entries()) {
-                    parsedURL.params[pair[0]] = pair[1];
-            }
-            parsedURL.protocol = parsedURL.protocol.replace(/:/, "");
-            let parts = parsedURL.host.split('.');
-            let tld = parts.pop().toLowerCase();
-            let domain = parts.pop();
-            parsedURL.domain = (domain + '.' + tld).toLowerCase();
-            if (!parsedURL.pathname || !parsedURL.pathname.length || parsedURL.pathname == '/') { // this is a topLevel link so will save to the domain table too along with ACLs
-                    parsedURL.topLevel = parsedURL.host;
-            }
-            let hostParts = parsedURL.host.split('.');
-            parsedURL.server = hostParts.shift();
-            if (! parsedURL.server.length) { parsedURL.server = '*' }
-            parsedURL.subdomain = hostParts.join('.');
+        }
+        // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+        parsedURL.params = {};
+        for (let pair of parsedURLObj.searchParams.entries()) {
+            parsedURL.params[pair[0]] = pair[1];
+        }
+        parsedURL.protocol = parsedURL.protocol.replace(/:/, "");
+        let parts = parsedURL.host.split('.');
+        let tld = parts.pop().toLowerCase();
+        let domain = parts.pop();
+        parsedURL.domain = (domain + '.' + tld).toLowerCase();
+        if (!parsedURL.pathname || !parsedURL.pathname.length || parsedURL.pathname == '/') { // this is a topLevel link so will save to the domain table too along with ACLs
+            parsedURL.topLevel = parsedURL.host;
+        }
+        let hostParts = parsedURL.host.split('.');
+        parsedURL.server = hostParts.shift();
+        if (!parsedURL.server.length) { parsedURL.server = '*' }
+        parsedURL.subdomain = hostParts.join('.');
 
-            if (parsedURL.search) {
-                    parsedURL.search = parsedURL.search.replace(/^\?/,'');
-            }
+        if (parsedURL.search) {
+            parsedURL.search = parsedURL.search.replace(/^\?/, '');
+        }
 
-            parsedURL.site = parsedURL.host;
+        parsedURL.site = parsedURL.host;
 
-            // try to determine the path to this resource. If a resource has an extension indicated by a '.' in name, remove that from path
-            if (parsedURL.pathname && parsedURL.pathname != '/' && parsedURL.pathname.length > 1) {
-                    let pathParts = parsedURL.pathname.split('/');
-                    if (pathParts.length && pathParts[pathParts.length-1] && pathParts[pathParts.length-1].length && pathParts[pathParts.length-1].match(/\./)) {
-                            parsedURL.file = pathParts.pop();
-                            let p = parsedURL.file.split(/\./);
-                            if (p.length > 1) {
-                                    parsedURL.extension = p.pop().toLowerCase();
-                            }
-                    }
-                    parsedURL.path = pathParts.join('/');
+        // try to determine the path to this resource. If a resource has an extension indicated by a '.' in name, remove that from path
+        if (parsedURL.pathname && parsedURL.pathname != '/' && parsedURL.pathname.length > 1) {
+            let pathParts = parsedURL.pathname.split('/');
+            if (pathParts.length && pathParts[pathParts.length - 1] && pathParts[pathParts.length - 1].length && pathParts[pathParts.length - 1].match(/\./)) {
+                parsedURL.file = pathParts.pop();
+                let p = parsedURL.file.split(/\./);
+                if (p.length > 1) {
+                    parsedURL.extension = p.pop().toLowerCase();
+                }
             }
+            parsedURL.path = pathParts.join('/');
+        }
 
-            // put the ID's in
-            parsedURL.url = url;
-            // sometimes, parser addes a trailing / which screws up stuff when we just want to try all the differeted top level permutations so we just want the subnitted URL
-            // however, if we subit an origin, typically, when getting absolte URL from a link,then we need to use the result href since it is calculated to be the absolute URL
-            if (parsedURL.pathname == '/' && ! parsedURL.url.match(/\/$/)) {
-                    parsedURL.path = '';
-            }
-            else {
-                    parsedURL.path = parsedURL.pathname;
-            }
-            //tracer.log(parsedURL);
-            return parsedURL;
-    }
-    catch(error) {
+        // put the ID's in
+        parsedURL.url = url;
+        // sometimes, parser addes a trailing / which screws up stuff when we just want to try all the differeted top level permutations so we just want the subnitted URL
+        // however, if we subit an origin, typically, when getting absolte URL from a link,then we need to use the result href since it is calculated to be the absolute URL
+        if (parsedURL.pathname == '/' && !parsedURL.url.match(/\/$/)) {
+            parsedURL.path = '';
+        } else {
+            parsedURL.path = parsedURL.pathname;
+        }
+        //tracer.log(parsedURL);
+        return parsedURL;
+    } catch (error) {
         tracer.log(error);
         return error;
     }
 }
-
-
-
---
+++ static/js/ajax.js
@@ -0,0 +1,29 @@
+function ajaxJSON(url, data, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    let urlGet = url + '?';
+    let delim = '';
+    for (var key in data) {
+        urlGet += delim + key + '=' + data[key];
+        delim = '&';
+    }
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+            if (xmlhttp.status == 200) {
+                var json = {};
+                try {
+                    var json = JSON.parse(xmlhttp.responseText);
+                } catch (error) {
+                    json.error = error.message;
+                }
+                callback(json);
+            } else {
+                callback({
+                    error: 'HTTP status: ' + xmlhttp.status
+                })
+            }
+        }
+    };
+    xmlhttp.open("GET", urlGet, true);
+    xmlhttp.send();
+}
